9d011dd2066956661540fc834239d9db04637be3
Xlebynya <gamemade152@gmail.com>
2026-08-04 20:47:14 +0300

    chore: удалены файлы из проекта

--- src/views/KnowledgeBase/useKnowledgeBase.ts
+++ src/views/KnowledgeBase/useKnowledgeBase.ts
@@ -14,54 +14,59 @@ interface CategoryItem {
     subtitle: string | null
 }
 
+const categories = ref<Category[]>([])
+const categoryItems = ref<CategoryItem[]>([])
+const categoriesLoading = ref(false)
+const categoryItemsLoading = ref(false)
+
+const fetchCategories = async () => {
+    categoriesLoading.value = true
+    try {
+        const response = await fetch(`/KnowledgeBase/Categories.json`)
+        if (!response.ok) {
+            throw new Error(`Ошибка загрузки: ${response.statusText}`)
+        }
+        categories.value = await response.json()
+    } catch (error) {
+        categories.value = []
+        console.error('Ошибка при загрузке JSON:', error)
+    } finally {
+        categoriesLoading.value = false
+    }
+}
+const fetchCategoryItems = async (category: string) => {
+    categoryItemsLoading.value = true
+    try {
+        const response = await fetch(`/KnowledgeBase/CategoryItems.json`)
+        if (!response.ok) {
+            throw new Error(`Ошибка загрузки: ${response.statusText}`)
+        }
+        const data = await response.json()
+        categoryItems.value = data[category]
+    } catch (error) {
+        categoryItems.value = []
+        console.error('Ошибка при загрузке JSON:', error)
+    } finally {
+        categoryItemsLoading.value = false
+    }
+}
+
 export function useknowledgeBase() {
     const route = useRoute()
-    const isRoot = computed(() => route.path === '/knowledge');
-    const categories = ref<Category[]>([])
-    const categoryItems = ref<CategoryItem[]>([])
-    const categoriesLoading = ref(false)
-    const categoryItemsLoading = ref(false)
+    const isRoot = computed(() => route.path === '/knowledge')
 
-    const fetchCategories = async () => {
-        categoriesLoading.value = true
-        try {
-            const response = await fetch(
-                `/KnowledgeBase/Categories.json`
-            )
-            if (!response.ok) {
-                throw new Error(`Ошибка загрузки: ${response.statusText}`)
-            }
-            const data = await response.json()
-            categories.value = data
-        } catch (error) {
-            categories.value = []
-            console.error('Ошибка при загрузке JSON:', error)
-        } finally {
-            categoriesLoading.value = false
-        }
-    }
-    const fetchCategoryItems = async (category: string) => {
-        categoryItemsLoading.value = true
-        try {
-            const response = await fetch(
-                `/KnowledgeBase/CategoryItems.json`
-            )
-            if (!response.ok) {
-                throw new Error(`Ошибка загрузки: ${response.statusText}`)
-            }
-            const data = await response.json()
-            categoryItems.value = data[category]
-        } catch (error) {
-            categoryItems.value = []
-            console.error('Ошибка при загрузке JSON:', error)
-        } finally {
-            categoryItemsLoading.value = false
-        }
-    }
+    const categoryTitle = computed(() => {
+        return categories.value.find(el => el.path === route.path)?.title
+    })
 
-    const categoryTitle = computed(()=> {return categories.value.find(el => el.path === route.path)?.title})
-
-    watch(() => route.params.category, () => {fetchCategoryItems(route.params.category as string); fetchCategories();}, {immediate: true})
+    watch(
+        () => route.params.category,
+        () => {
+            fetchCategoryItems(route.params.category as string)
+            fetchCategories()
+        },
+        { immediate: true }
+    )
 
     return {
         categoryItems,
@@ -70,6 +75,6 @@ export function useknowledgeBase() {
         categoriesLoading,
         isRoot,
         categoryTitle,
-        fetchCategories
+        fetchCategories,
     }
 }
